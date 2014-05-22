4e8048b0137e3a52e9b5f454ef0382c69a4a48e0
Christian Speckner <cnspeckn@googlemail.com>
2014-05-22 08:30:39 +0200

    Font tests.

--- specs/sheetSpec.js
+++ specs/sheetSpec.js
@@ -1,14 +1,132 @@
-var xl = require('../lib/libxl');
+var xl = require('../lib/libxl'),
+    util = require('util');
+
+function shouldThrow(fun, scope) {
+    var args = Array.prototype.slice.call(arguments, 2);
+
+    expect(function() {fun.apply(scope, args);}).toThrow();
+}
 
 describe('The font class', function() {
 
     var book = new xl.Book(xl.BOOK_TYPE_XLS),
-        font = book.addFont(),
-        wrongBook = new xl.Book(xl.BOOK_TYPE_XLS),
-        wrongFont = book.addFont();
+        font = book.addFont();
 
     it('The font constructor can not be called directly', function() {
         expect(function() {new font.constructor();}).toThrow();
+    });
+
+    it('font.size reads font size', function() {
+        shouldThrow(font.size, book);
+        font.setSize(10);
+        expect(font.size()).toBe(10);
+        font.setSize(20);
+        expect(font.size()).toBe(20);
+    });
+
+    it('font.setSize sets font size', function() {
+        shouldThrow(font.setSize, font, '');
+        shouldThrow(font.setSize, {}, 10);
+        expect(font.setSize(10)).toBe(font);
+    });
+
+    it('font.italic checks wether a font is italic', function() {
+        shouldThrow(font.italic, book);
+        font.setItalic(false);
+        expect(font.italic()).toBe(false);
+        font.setItalic(true);
+        expect(font.italic()).toBe(true);
+    });
+
+    it('font.setItalic toggles a font italic', function() {
+        shouldThrow(font.setItalic, font, 10);
+        shouldThrow(font.setItalic, book, true);
+        expect(font.setItalic(true)).toBe(font);
+    });
+
+    it('font.strikeOut checks wether a font is striked out', function() {
+        shouldThrow(font.strikeOut, book);
+        font.setStrikeOut(false);
+        expect(font.strikeOut()).toBe(false);
+        font.setStrikeOut(true);
+        expect(font.strikeOut()).toBe(true);
+    });
+
+    it('font.setStrikeOut toggles a font striked out', function() {
+        shouldThrow(font.setStrikeOut, font, 10);
+        shouldThrow(font.setStrikeOut, book, true);
+        expect(font.setStrikeOut(true)).toBe(font);
+    });
+   
+
+    it('font.color gets font color', function() {
+        shouldThrow(font.color, book);
+        font.setColor(xl.COLOR_BLACK);
+        expect(font.color()).toBe(xl.COLOR_BLACK);
+        font.setColor(xl.COLOR_WHITE);
+        expect(font.color()).toBe(xl.COLOR_WHITE);
+    });
+
+    it('font.setColor sets font color', function() {
+        shouldThrow(font.setColor, font, true);
+        shouldThrow(font.setColor, book, xl.COLOR_BLACK);
+        expect(font.setColor(xl.COLOR_BLACK)).toBe(font);
+    });
+
+    it('font.bold checks whether a font is bold', function() {
+        shouldThrow(font.bold, book);
+        font.setBold(false);
+        expect(font.bold()).toBe(false);
+        font.setBold(true);
+        expect(font.bold()).toBe(true);
+    });
+
+    it('font.setBold toggles a font bold', function() {
+        shouldThrow(font.setBold, font, 10);
+        shouldThrow(font.setBold, book, true);
+        expect(font.setBold(true)).toBe(font);
+    });
+
+    it('font.script gets a fonts script style', function() {
+        shouldThrow(font.script, book);
+        font.setScript(xl.SCRIPT_NORMAL);
+        expect(font.script()).toBe(xl.SCRIPT_NORMAL);
+        font.setScript(xl.SCRIPT_SUPER);
+        expect(font.script()).toBe(xl.SCRIPT_SUPER);
+    });
+
+    it('font.setScript sets a fonts script style', function() {
+        shouldThrow(font.setScript, font, '');
+        shouldThrow(font.setScript, book, xl.SCRIPT_NORMAL);
+        expect(font.setScript(xl.SCRIPT_NORMAL)).toBe(font);
+    });
+
+    it('font.underline gets a fonts underline mode', function() {
+        shouldThrow(font.underline, book);
+        font.setUnderline(xl.UNDERLINE_NONE);
+        expect(font.underline()).toBe(xl.UNDERLINE_NONE);
+        font.setUnderline(xl.UNDERLINE_SINGLE);
+        expect(font.underline()).toBe(xl.UNDERLINE_SINGLE);
+    });
+
+    it('font.setUnderline sets a fonts underline mode', function() {
+        shouldThrow(font.setUnderline, font, true);
+        shouldThrow(font.setUnderline, book, xl.UNDERLINE_NONE);
+        expect(font.setUnderline(xl.UNDERLINE_NONE)).toBe(font);
+    });
+
+    it('font.name gets the font name', function() {
+        shouldThrow(font.name, book);
+        font.setName('arial');
+        expect(font.name()).toBe('arial');
+        font.setName('times');
+        expect(font.name()).toBe('times');
+    });
+
+    it('font.setName sets the font name', function() {
+        shouldThrow(font.setName, font, 10);
+        shouldThrow(font.setName, book, 'arial');
+        expect(font.setName('arial')).toBe(font);
     });
 
 });
@@ -43,7 +161,7 @@ describe('The sheet class', function() {
             .writeFormula(row, 1, '=1');
 
         expect(function() {sheet.isFormula();}).toThrow();
-        expect(function() {sheet.isFormula.call({}, row, 0)}).toThrow();
+        expect(function() {sheet.isFormula.call({}, row, 0);}).toThrow();
 
         expect(sheet.isFormula(row, 0)).toBe(false);
         expect(sheet.isFormula(row, 1)).toBe(true);
@@ -269,7 +387,7 @@ describe('The sheet class', function() {
 
     it('sheet.setRowHidden hides or shows a row', function() {
         expect(function() {sheet.setRowHidden();}).toThrow();
-        expect(function() {sheet.setRowHidden.call({}, row, true)}).toThrow();
+        expect(function() {sheet.setRowHidden.call({}, row, true);}).toThrow();
 
         expect(sheet.setRowHidden(row, true)).toBe(sheet);
         expect(sheet.rowHidden(row)).toBe(true);
@@ -286,12 +404,12 @@ describe('The sheet class', function() {
 
     it('sheet.setColHidden hides or shows a column', function() {
         expect(function() {sheet.setColHidden();}).toThrow();
-        expect(function() {sheet.setColHidden.call({}, 0, true)}).toThrow();
+        expect(function() {sheet.setColHidden.call({}, 0, true);}).toThrow();
 
         expect(sheet.setColHidden(0, true)).toBe(sheet);
         expect(sheet.colHidden(0)).toBe(true);
         expect(sheet.setColHidden(0, false)).toBe(sheet);
         expect(sheet.colHidden(0)).toBe(false);
-    })
+    });
 
 });
