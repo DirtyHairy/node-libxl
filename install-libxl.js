d6a84fab2ca1ab67f220ba39217c9833469b3046
Christian Speckner <cnspeckn@googlemail.com>
2014-07-30 09:27:27 +0200

    Fix a race in API tarball download.
    (cherry picked from commit c626569035769345e2422dc75b0cd08702c9493f)

--- install-libxl.js
+++ install-libxl.js
@@ -124,7 +124,7 @@ var download = function(callback) {
 
             writer.on('error', onError);
             stream.on('error', onError);
-            stream.on('end', function() {
+            writer.on('close', function() {
                 ftpClient.end();
 
                 console.log('Download complete!');
