8db59a6524112a8d343a3eebc93a9e4b6a062b1a
Christian Speckner <christian.speckner@mayflower.de>
2020-06-20 11:00:59 +0200

    Download and extract dmg on mac.

--- install-libxl.js
+++ install-libxl.js
@@ -1,19 +1,19 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2013 Christian Speckner <cnspeckn@googlemail.com>,
  *                    Torben Fitschen <teddyttn@gmail.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,6 +24,7 @@
  */
 
 var fs = require('fs'),
+    childProcess = require('child_process'),
     os = require('os'),
     path = require('path'),
     tmp = require('tmp'),
@@ -31,6 +32,7 @@ var fs = require('fs'),
     md5 = require('md5'),
     zlib = require('zlib'),
     tar = require('tar'),
+    plist = require('plist'),
     https = require('https'),
     AdmZip = require('adm-zip');
 
@@ -40,7 +42,7 @@ var isWin = !!os.platform().match(/^win/),
     libxlDir = path.join(dependencyDir, 'libxl'),
     archiveEnv = 'NODE_LIBXL_SDK_ARCHIVE';
 
-var download = function(callback) {
+function download(callback) {
     function getPlaform() {
         if (isWin) {
             return 'win';
@@ -53,8 +55,21 @@ var download = function(callback) {
         return 'lin';
     }
 
+    function getArchiveSuffix() {
+        switch (getPlaform()) {
+            case 'win':
+                return 'zip';
+
+            case 'mac':
+                return 'dmg';
+
+            default:
+                return 'tar.gz';
+        }
+    }
+
     function getArchiveName() {
-        return util.format('libxl-%s-latest.%s', getPlaform(), isWin ? 'zip' : 'tar.gz');
+        return util.format('libxl-%s-latest.%s', getPlaform(), getArchiveSuffix());
     }
 
     function downloadToFile(filename, callback) {
@@ -121,7 +136,7 @@ var download = function(callback) {
     });
 };
 
-var downloadIfNecessary = function(callback) {
+function downloadIfNecessary(callback) {
     var suppliedArchive = process.env[archiveEnv];
 
     if (suppliedArchive) {
@@ -134,19 +149,20 @@ var downloadIfNecessary = function(callback) {
     }
 };
 
-var extractor = function(file, target, callback) {
-    console.log('Extracting ' + file + ' ...');
+function findSubdir(dir, pattern) {
+    var files = fs.readdirSync(dir),
+        i,
+        file;
+      for (i = 0; i < files.length; i++) {
+          file = files[i];
+          if (file.match(pattern)) {
+              return path.join(dir, file);
+          }
+      }
+      return null;
+  };
 
-    if (file.match(/\.zip$/)) {
-        extractZip(file, target, callback);
-    } else if (file.match(/\.tar\.gz/)) {
-        extractTgz(file, target, callback);
-    } else {
-        callback(new Error('unnown archive format'));
-    }
-};
-
-var extractTgz = function(archive, destination, callback) {
+function extractTgz(archive, destination, callback) {
     var fileStream = fs.createReadStream(archive),
         decompressedStream = fileStream.pipe(zlib.createGunzip()),
         untarStream = tar.Extract({path: destination});
@@ -154,7 +170,7 @@ var extractTgz = function(archive, destination, callback) {
     untarStream.on('end', function() {
         callback();
     });
-    
+
     [fileStream, decompressedStream, untarStream].forEach(function(stream) {
         stream.on('error', function(e) {
          callback(e);
@@ -164,7 +180,7 @@ var extractTgz = function(archive, destination, callback) {
     decompressedStream.pipe(untarStream);
 };
 
-var extractZip = function(archive, destination, callback) {
+function extractZip(archive, destination, callback) {
     var zip;
 
     try {
@@ -177,17 +193,81 @@ var extractZip = function(archive, destination, callback) {
     }
 };
 
-var finder = function(dir, pattern) {
-  var files = fs.readdirSync(dir),
-      i,
-      file;
-    for (i = 0; i < files.length; i++) {
-        file = files[i];
-        if (file.match(pattern)) {
-            return path.join(dir, file);
+function extractDmg(image, destination, callback) {
+    childProcess.execFile('hdiutil', ['mount', '-plist', image], {encoding: 'utf8'}, function(err, stdout) {
+        var result, candidates, i, mountpoint, sdkdir;
+
+        function unmount(cb) {
+            var tries = 0;
+
+            function work() {
+                childProcess.execFile('hdiutil', ['unmount', mountpoint], function(err) {
+                    if (err && tries++ < 5) {
+                        setTimeout(work, 1000);
+                    } else {
+                        cb(err);
+                    }
+                });
+            }
+
+            if (mountpoint) {
+                work();
+            } else {
+                cb();
+            }
         }
+
+        if (err) {
+            callback(new Error(util.format("failed to mount %s: %s", image, err.message)));
+            return;
+        }
+
+        try {
+            result = plist.parse(stdout);
+
+            candidates = result['system-entities'];
+            if (!candidates) throw new Error();
+
+            for (i = 0; i < candidates.length; i++) {
+                if (mountpoint = candidates[i]['mount-point']) break;
+            }
+
+            if (!mountpoint) throw Error();
+        } catch (e) {
+            callback(new Error('failed to identify dmg mountpoint'));
+
+            return;
+        }
+
+        sdkdir = findSubdir(mountpoint, /^libxl/);
+        if (!sdkdir) {
+            unmount(function() {
+                callback(new Error('SDK not found in dmg'));
+            });
+
+            return;
+        }
+
+        childProcess.execFile('cp', ['-r', findSubdir(mountpoint, /^libxl/), destination], function(err) {
+            unmount(function() {
+                callback(err);
+            });
+        });
+    })
+}
+
+function extractArchive(file, target, callback) {
+    console.log('Extracting ' + file + ' ...');
+
+    if (file.match(/\.zip$/)) {
+        extractZip(file, target, callback);
+    } else if (file.match(/\.tar\.gz/)) {
+        extractTgz(file, target, callback);
+    } else if (file.match(/\.dmg$/)) {
+        extractDmg(file, target, callback);
+    } else {
+        callback(new Error('unnown archive format'));
     }
-    return null;
 };
 
 if (fs.existsSync(libxlDir)) {
@@ -200,7 +280,7 @@ if (!fs.existsSync(dependencyDir)) {
 }
 
 downloadIfNecessary(function(archive) {
-    extractor(archive, dependencyDir, function(e) {
+    extractArchive(archive, dependencyDir, function(e) {
         if (e) {
             console.error(e.message || 'Extraction failed');
             process.exit(1);
@@ -208,7 +288,7 @@ downloadIfNecessary(function(archive) {
 
         if (!process.env[archiveEnv]) fs.unlinkSync(archive);
 
-        var extractedDir = finder(dependencyDir, /^libxl/);
+        var extractedDir = findSubdir(dependencyDir, /^libxl/);
         console.log('Renaming ' + extractedDir + ' to ' + libxlDir + ' ...');
 
         fs.renameSync(extractedDir, libxlDir);
