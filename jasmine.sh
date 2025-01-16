1dce7bf88e2d8aba95794dfe34d575973abb6599
Christian Speckner <christian.speckner@mayflower.de>
2025-01-16 22:04:10 +0100

    Support x86 and x64

--- jasmine.sh
+++ jasmine.sh
@@ -10,6 +10,10 @@ elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = aarch64 ]; then
     LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib-aarch64" $JASMINE
 elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = armv7l ]; then
     LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib-armhf" $JASMINE
+elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = i686 ]; then
+    LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib" $JASMINE
+elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = x86_64 ]; then
+    LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib64" $JASMINE
 else
     echo unsupported system
 fi
