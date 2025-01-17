3347a05018c740d6ee231d8dfe44f437c77e981e
Christian Speckner <christian.speckner@mayflower.de>
2025-01-17 19:35:02 +0100

    Better portability.

--- jasmine.sh
+++ jasmine.sh
@@ -5,15 +5,15 @@ MACHINE=$(uname -m)
 JASMINE="node ./node_modules/.bin/jasmine --config=jasmine.json"
 
 if [ "$SYSTEM" = Darwin ]; then
-    DYLD_LIBRARY_PATH="$(pwd)/deps/libxl/lib" $JASMINE
+    DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$(pwd)/deps/libxl/lib" $JASMINE
 elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = aarch64 ]; then
-    LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib-aarch64" $JASMINE
+    LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/deps/libxl/lib-aarch64" $JASMINE
 elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = armv7l ]; then
-    LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib-armhf" $JASMINE
+    LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/deps/libxl/lib-armhf" $JASMINE
 elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = i686 ]; then
-    LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib" $JASMINE
+    LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/deps/libxl/lib" $JASMINE
 elif [ "$SYSTEM" = Linux ] && [ "$MACHINE" = x86_64 ]; then
-    LD_LIBRARY_PATH="$(pwd)/deps/libxl/lib64" $JASMINE
+    LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/deps/libxl/lib64" $JASMINE
 else
     echo unsupported system
 fi
